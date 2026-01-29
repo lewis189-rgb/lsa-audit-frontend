b53838a35dacf57d58f95aa29e4b6e21b027c03a
lewis189-rgb <lewis189@gmail.com>
2026-01-29 14:51:03 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LSAAuditTool from './LSAAuditTool'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LSAAuditTool />
+  </React.StrictMode>,
+)
